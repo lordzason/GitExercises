9e914ba0bed810d776030faf667cbf8113482953
Zhi <chenzhi17@grinnell.edu>
2014-09-01 16:31:14 -0500

    Committing "Hello, Github!"

---
+++ src/edu/grinnell/csc207/chenzhi17/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package edu.grinnell.csc207.chenzhi17.hello;
+
+public class HelloWorld
+{
+  
+  public static void 
+  main (String[] args) 
+{
+  System.out.println ("Hello, GitHub!");
+} // main(String[])
+  
+}
